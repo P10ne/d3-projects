2b7bfd101eacf2ee99f58c398daeba8249bc0103
alexander <i-smart97@mail.ru>
2021-11-29 16:36:15 +0400

    add forceCollide

--- src/pages/tree/index.ts
+++ src/pages/tree/index.ts
@@ -1,4 +1,15 @@
 import './styles.scss';
 import { init } from "./tree";
+import { Toolbar } from "./toolbar/Toolbar";
+
+(function initToolbar() {
+  const toolbar = new Toolbar();
+  document.body.appendChild(toolbar.node);
+})();
+
+
+
+
 
 init();
+
